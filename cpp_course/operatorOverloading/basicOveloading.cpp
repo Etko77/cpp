81f44e3753b826642bee55528c9e07d7541fed2d
Etko77 <etienne1800503@gmail.com>
2025-01-21 22:37:12 +0200

    Extraction adn isnertion operations

--- cpp_course/operatorOverloading/basicOveloading.cpp
+++ cpp_course/operatorOverloading/basicOveloading.cpp
@@ -4,13 +4,34 @@ using namespace std;
 #include <cstring>
 
 class Mystring{
+
+    friend ostream &operator<<(ostream &os, const Mystring &rso);
+    friend istream &operator>>(istream &in, Mystring &rso);
     private:
         char* str;
     public:
+
+        Mystring(){
+            str = new char[1];
+            str[0] = '\0';
+        }
         Mystring(const char * s){
             str = new char[strlen(s)+1];
             strcpy(str,s);
         }
+        Mystring(const Mystring &other){
+            str = new char[strlen(other.str)+1];
+            strcpy(str,other.str);
+
+        }
+        Mystring& operator=(const Mystring &other) { 
+        if (this != &other) {
+            delete[] str; 
+            str = new char[strlen(other.str) + 1];
+            strcpy(str, other.str);
+        }
+        return *this;
+    }
         ~Mystring(){
             delete[] str;
         }
@@ -22,9 +43,30 @@ class Mystring{
                 return false;
             }
         }
-        friend ostream& operator<<(ostream& os, const Mystring& myStr){
-            os << myStr.str;
-            return os;
+        Mystring operator+(const Mystring &rhs){
+            size_t buffSize = strlen(this->str)+strlen(rhs.str)+1;
+            char* buff = new char[buffSize];
+            strcpy(buff,this->str);
+            strcat(buff,rhs.str);
+            Mystring temp {buff};
+            delete [] buff;
+            return temp;
+            
+        }
+        Mystring operator-(){
+            char* buff = new char[strlen(this->str)+1];
+            strcpy(buff,this->str);
+            for(size_t i = 0;i<strlen(buff);i++){
+                buff[i] = tolower(buff[i]);
+            }
+            Mystring temp{buff};
+            delete [] buff;
+            return temp;
+
+        }
+        
+        void display(){
+            cout<< str<<endl;
         }
         
 
@@ -36,6 +78,26 @@ int main(){
     Mystring alberto{"Alberto"};
     Mystring alfredoMartini{"Alfredo"};
     cout << (alfredo == alfredoMartini) << endl;
-
+    alfredo.display();
+    // alfredo = -alfredo;
+    // alfredo.display();
+    Mystring bob;
+    cout<<"Enter a string for the object bob:"<<endl;
+    cin >> bob;
+    cout<<"Bob has entered the game "<<bob<<endl;
     return 0;
 }
+
+//extraction operator
+ostream &operator<<(ostream &os, const Mystring &rhs){
+    os << rhs.str;
+    return os;
+};
+//insertion operator
+istream &operator>>(istream &in, Mystring &rhs){
+    char* buff = new char[1000];
+    in >> buff;
+    rhs = Mystring{buff};
+    delete [] buff;
+    return in;
+};
